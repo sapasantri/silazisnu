da1330db1e4de7bff6f31fe8f0f65c07260cf8fd
niandevOS <azharnian@gmail.com>
2020-09-20 15:54:38 +0700

    add model Kota

--- apps/wilayah/models.py
+++ apps/wilayah/models.py
@@ -11,3 +11,14 @@ class Propinsi(SilazisnuModel):
 
     def __str__(self):
         return str(self.kode + self.nama)
+
+class Kota(SilazisnuModel):
+    propinsi = models.ForeignKey(Propinsi, on_delete=models.CASCADE)
+    kode = models.IntegerField(blank=False, null=False)
+    nama = models.CharField(max_length=250, blank=False, null=False)
+
+    class Meta:
+        verbose_name = _('Kota')
+
+    def __str__(self):
+        return str(self.kode + self.nama)
