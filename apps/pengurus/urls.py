8085f3580cc2cacd5eb7895a907d2f2ec9c51abe
Azhar <azharnian@gmail.com>
2020-10-08 20:40:40 +0700

    Update urls.py
    add refresh token

--- apps/pengurus/urls.py
+++ apps/pengurus/urls.py
@@ -2,6 +2,10 @@ from rest_framework import routers
 from django.urls import include, path
 from django.conf.urls import url
 from . import api
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = routers.DefaultRouter()
 router.register(r'pengurus', api.PengurusViewSet)
@@ -10,5 +14,6 @@ router.register(r'pengurus', api.PengurusViewSet)
 app_name = 'pengurus'
 urlpatterns = [
     path('pengurus/login/', api.LoginViewSet.as_view(), name='login_token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('organisasi/', include(router.urls)),
 ]
